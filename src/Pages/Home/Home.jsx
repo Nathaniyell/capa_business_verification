3ad224b0dd2a777cb296c85304939ea97cfc13a0
Nathaniyell <essien.nathan@yahoo.com>
2024-01-13 20:52:26 +0100

    update Footer.jsx, Home.jsx and HomeCards.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -57,7 +57,7 @@ const Home = () => {
         <h2 className="text-center text-2xl my-10 font-semibold">
           Reliable end-to-end verification services
         </h2>
-        <section className="w-11/12 lg:w-10/12 mx-auto p-2 flex flex-grow flex-col md:flex-row gap-16 md:gap-10  justify-between">
+        <section className="w-11/12 lg:w-10/12 mx-auto p-2 flex flex-grow flex-col md:flex-row gap-20 md:gap-16  justify-between">
           {cardDetails.map((card, index) => {
             return (
               <HomeCards
--- src/Pages/Home/HomeCards.jsx
+++ src/Pages/Home/HomeCards.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Button from "../../Components/Button";
 export const HomeCards = ({ title, description, linkDestination, avatar }) => {
   return (
-    <div className="p-3 lg:p-8 md:w-2/3 bg-teal-50 border border-green-900 rounded-md flex flex-col flex-grow">
+    <div className="p-3 lg:p-8 md:w-2/3 bg-teal-50 border shadow-lg border-green-900 rounded-md flex flex-col flex-grow">
       <div className="mx-auto w-2/5 md:w-1/3 place-items-center grid">
         <img src={avatar} alt="card-img" className="md:w-[120px]" />
       </div>
